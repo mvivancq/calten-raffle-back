38721b94d845007061a567664a0452d2e733d604
MartinAntonio123 <mavp98@hotmail.com>
2024-03-11 14:01:19 -0600

    add put payment result

--- api/utils/databaseStorage.js
+++ api/utils/databaseStorage.js
@@ -39,25 +39,21 @@ export const getPaymentResult = async (schema) => {
 }
 
 export const putPaymentResult = async (schema) => {
-    // console.log('insert post request into db');
-    // const id = parseResultDetails(data).resultDetails.id;
-    // const idclient = 1;
-    // await db.insert({ 
-    //     id, 
-    //     idclient,
-    //     cellphone: schema.cellphone,
-    //     reference: schema.reference,
-    //     concept: schema.concept, 
-    //     amount: schema.amount,
-    //     beneficiaryname: schema.beneficiaryName,
-    //     beneficiaryaccount: schema.beneficiaryAccount,
-    //     status: -1,
-    // })
-    // .into('request')
-    // .then( () =>  console.log('success inserting the data'))
-    // .catch(err => {
-    //     console.log('error inserting the data');
-    //     console.log(err);
-    // });
+    return await db('raffle')
+    .returning("*")
+    .where('id', schema.reference)
+    .andWhere('commerceCertification', schema.commerceCertification)
+    .andWhere('amount', schema.amount)
+    .update({ status: schema.status })
+    .then( val =>  {
+            console.log('success updating the data');
+            return val;
+        }
+    )
+    .catch(err => {
+        console.log('error updating the data');
+        console.log(err);
+        return err;
+    });
     return 1;
 }
