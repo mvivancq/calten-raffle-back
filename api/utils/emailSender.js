aac5ffb5f5495f8b22a002bb230585410ded3804
MartinAntonio123 <mavp98@hotmail.com>
2024-09-23 13:10:58 -0600

    2 types of emails

--- api/utils/emailSender.js
+++ api/utils/emailSender.js
@@ -1,5 +1,6 @@
 import nodemailer from "nodemailer";
 import fs from "fs";
+import { constants } from "./constants.js";
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
@@ -12,14 +13,24 @@ const transporter = nodemailer.createTransport({
 });
 
 export const sendCaltenEmail = async (purchaseData) => {
-    const htmlTemplate = fs.readFileSync('./api/utils/emailTemplate.html', 'utf-8');
+    // Template 1
+    // const htmlTemplate = fs.readFileSync('./api/emailTemplates/template1.html', 'utf-8');
+
+    // const htmlContent = htmlTemplate
+    //     .replace('{{name}}', purchaseData.name)
+    //     .replaceAll('{{many}}', purchaseData.tickets > 1 ? 's' : '')
+    //     .replace('{{num}}', 'numero234')
+    //     .replace('{{tickets}}', purchaseData.tickets);
+
+    // Template 2
+    const htmlTemplate = fs.readFileSync('./api/emailTemplates/template2.html', 'utf-8');
+    const emailData = constants.emails[Math.floor(Math.random() * 3)];
 
     const htmlContent = htmlTemplate
-        .replace('{{name}}', purchaseData.name)
-        .replace('{{many}}', purchaseData.tickets > 1 ? 's' : '')
-        .replace('{{many}}', purchaseData.tickets > 1 ? 's' : '')
-        .replace('{{num}}', 'numero234')
-        .replace('{{tickets}}', purchaseData.tickets);
+        .replaceAll('{{name}}', purchaseData.name)
+        .replaceAll('{{member}}', emailData.member)
+        .replace('{{team}}', emailData.team)
+        .replace('{{photo}}', emailData.photo);
 
     var mailOptions = {
         from: process.env.CALTENMAIL,
