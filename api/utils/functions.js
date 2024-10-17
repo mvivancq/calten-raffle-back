0f79f4731a108dba837a6f80926bff4a07115d4b
MartinAntonio123 <mavp98@hotmail.com>
2024-10-16 19:34:13 -0600

    one

--- api/utils/functions.js
+++ api/utils/functions.js
@@ -2,7 +2,7 @@ import { constants } from "./constants.js";
 
 export function checkEmailDomain(email) {
     if (email.endsWith('@calten.mx')) {
-        return 0.1;
+        return 1;
     } else {
         return constants.ticketPrice;
     }
